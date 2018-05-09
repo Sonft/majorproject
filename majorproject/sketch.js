df34a380eea72b965ad51478117b982015e69e14
Sonft <36053930+Sonft@users.noreply.github.com>
2018-05-09 15:16:01 -0600

    rules

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -411,6 +411,25 @@ function backGroundThree() {
   fill(255);
   textSize(30);
   text("these are the rules", 10, 50);
+  //Player Combat works like dice rolls.
+  //Unit movement works depending on unit, tech, and number of squares moved.
+  //There are four large colonizing european nations.
+  //There are 58 provinces in total and each province counts as a certain amount of score.
+  //Gold, units, provinces, resources, all count towards the end score.
+  //The end score determines who wins the game.
+  //There are many ways to win the game, from colonizing rapidly
+  //To colonizing late and conquering your opponents.
+  //The game is meant to be a constant struggle and colonies can flip loyalty at any time.
+  //You spend gold and resources to gain units which help you conquer others.
+  //The game lasts 100 turns, from the 1700s till 1800.
+  //To colonize a province, simply --------------
+  //To move units, do ----------------
+  //To attack, select your own units, and then select an enemy unit within range.
+  //The dice rolls are based on chance, however, some units will have higher rolls than others based on cost.
+  //There are two types of units, one unit that can only move on land, and others that can only move on water.
+  //There will be units that can transport land units over water.
+  //The places that are blacked out are either uncolonizable countries, or future countries that will be added to the game.
+  //
 
   //the exit button
   if (windowWidth > mouseX && windowWidth - 50 < mouseX && 50 > mouseY && 0 < mouseY && mouseIsPressed) {
